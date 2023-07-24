37bdb1e25d9986baf5b007ec3d18fdad803164e1
Souvik chakraborty <22souvik22@gmail.com>
2023-07-24 23:33:39 +0530

    Complete design

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './My_components/Navbar';
 import Main from './My_components/Main';
-<<<<<<< HEAD
-
 
 function App() {
   return (
     <div className='bg-[#0f0f0f] h-screen overflow-x-hidden '>
-=======
-
-function App() {
-  return (
-    <div className='bg-black h-screen'>
->>>>>>> f955b4f7074a4ad5bba7591f69453338e24334a2
       <Navbar />
       <Main/>
     </div>
